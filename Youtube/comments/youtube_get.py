f50116e5edb7947151b813b59fcb7449a9f90671
ChoiInYeol <chldlsel@khu.ac.kr>
2021-10-14 20:46:38 +0900

    [ADD] model update

--- Youtube/comments/youtube_get.py
+++ Youtube/comments/youtube_get.py
@@ -7,8 +7,8 @@ from googleapiclient.discovery import build
 # get Google Youyube Api key : https://console.cloud.google.com/apis/dashboard
 
 comments = list()
-api_obj = build('youtube', 'v3', developerKey = YOUTUBEAPI)
-response = api_obj.commentThreads().list(part='snippet,replies', videoId=VIDEOID, maxResults=100).execute() #videoID is video's Only code
+api_obj = build('youtube', 'v3', developerKey = "AIzaSyBZPe4dDCn9xflZ_vFthMjey9KAPLj2jmM")
+response = api_obj.commentThreads().list(part='snippet,replies', videoId="hiv9IdOOIH0", maxResults=100).execute() #videoID is video's Only code
 
 while response:
     for item in response['items']:
@@ -21,10 +21,9 @@ while response:
                 comments.append([reply['textDisplay'], reply['authorDisplayName'], reply['publishedAt'], reply['likeCount']])
  
     if 'nextPageToken' in response:
-        response = api_obj.commentThreads().list(part='snippet,replies', videoId=VIDEOID, pageToken=response['nextPageToken'], maxResults=100).execute() #videoID is video's Only code
+        response = api_obj.commentThreads().list(part='snippet,replies', videoId='hiv9IdOOIH0', pageToken=response['nextPageToken'], maxResults=100).execute() #videoID is video's Only code
     else:
         break
 
 df = pandas.DataFrame(comments)
 df.to_excel('results.xlsx', header=['comment', 'author', 'date', 'num_likes'], index=None)
-
